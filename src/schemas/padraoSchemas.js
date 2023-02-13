a33b1869c182d17c83dbe952c37cee4da48400dd
jmarcossss <joaomalcantaraaa@gmail.com>
2023-02-13 02:18:40 -0300

    fix customers

--- src/schemas/padraoSchemas.js
+++ src/schemas/padraoSchemas.js
@@ -13,29 +13,3 @@ export const padraoSchemaCustomers = joi.object({
     cpf: joi.string().length(11).regex(/^[0-9]+$/).required(),
     birthday: joi.date().required(),
 });
-
-export const padraoSchemaRentals = joi.object({
-    customerId: joi.number().required(),
-    gameId: joi.number().required(),
-    rentDate: joi.date().required(),
-    daysRented: joi.number().required(),
-    returnDate: joi.date(),
-    originalPrice: joi.number().required(),
-    delayFee: joi.number(),
-  });
-  
-export const padraoSchemaRentals2 = joi.object({
-    customerId: joi
-      .number()
-      .integer()
-      .min(1)
-      .max(Number.MAX_SAFE_INTEGER)
-      .required(),
-    gameId: joi.number().integer().min(1).max(Number.MAX_SAFE_INTEGER).required(),
-    daysRented: joi
-      .number()
-      .integer()
-      .min(1)
-      .max(Number.MAX_SAFE_INTEGER)
-      .required(),
-});
